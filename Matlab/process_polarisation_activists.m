4a70ffcda52a81fe8932674b2f5c1fa113244dba
christopher <christopher.coding@deepsea.net>
2024-09-21 19:38:09 +0100

    parameter changes
    Some changes to the data collection files

--- Matlab/process_polarisation_activists.m
+++ Matlab/process_polarisation_activists.m
@@ -1,9 +1,9 @@
-data = table();
+opendata = table();
 
 activistParameters = [-0.4 -0.2 0 0.2 0.4 0.6 0.8 1 2 3 4:2:10];
 
 for i = 1:length(activistParameters)
-   system("python main.py 1500 -H -p2 " + activistParameters(i));
+   system("python main.py 2000 --line -H -p1 0 -p2 " + activistParameters(i));
     p = get_mean_polarisation("output.dat");
     data.turnoutParameter(i) = NaN;
     data.activistParameter(i) = activistParameters(i);
@@ -11,13 +11,13 @@ for i = 1:length(activistParameters)
 end
 
 for i = 1:length(activistParameters)
-   system("python main.py 1500 -H -p2 " + activistParameters(i));
+   system("python main.py 2000 --line -H -p1 0 -p2 " + activistParameters(i));
     p = get_mean_polarisation("output.dat");
     data.Polarisation2(i) = p;
 end
 
 for i = 1:length(activistParameters)
-   system("python main.py 1500 -H -p2 " + activistParameters(i));
+   system("python main.py 2000 --line -H -p1 0 -p2 " + activistParameters(i));
     p = get_mean_polarisation("output.dat");
     data.Polarisation3(i) = p;
 end
--- Matlab/process_polarisation_turnout.m
+++ Matlab/process_polarisation_turnout.m
@@ -1,9 +1,9 @@
 data = table();
 
-turnoutParameters = [0 0.5 1 1.5 2:1:12];
+turnoutParameters = [0 0.5 1 1.5 2:2:16 24];
 
 for i = 1:length(turnoutParameters)
-    system("python main.py 1500 -H -p2 0 -p1 " + turnoutParameters(i));
+    system("python main.py 2000 -H --line -p2 0 -p1 " + turnoutParameters(i));
     p = get_mean_polarisation("output.dat");
     data.turnoutParameter(i) = turnoutParameters(i);
     data.activistParameter(i) = 0;
@@ -11,13 +11,13 @@ for i = 1:length(turnoutParameters)
 end
 
 for i = 1:length(turnoutParameters)
-    system("python main.py 1500 -H -p2 0 -p1 " + turnoutParameters(i));
+    system("python main.py 2000 -H --line -p2 0 -p1 " + turnoutParameters(i));
     p = get_mean_polarisation("output.dat");
     data.Polarisation2(i) = p;
 end
 
 for i = 1:length(turnoutParameters)
-    system("python main.py 1500 -H -p2 0 -p1 " + turnoutParameters(i));
+    system("python main.py 2000 -H --line -p2 0 -p1 " + turnoutParameters(i));
     p = get_mean_polarisation("output.dat");
     data.Polarisation3(i) = p;
 end
